147c5f0a89be12798cac55c847986062f8efed61
Jhimy2206 <jimibanda123456789@gmail.com>
2025-11-11 02:48:16 -0500

    Agregando las capas para el modelo prestamo

--- libreria-backend/src/main/java/com/example/demo/entity/Prestamo.java
+++ libreria-backend/src/main/java/com/example/demo/entity/Prestamo.java
@@ -1,4 +1,36 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Prestamo {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime fechaPrestamo;
+
+    private LocalDateTime fechaDevolucion;
+
+    @Column(nullable = false)
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+
+
 }
